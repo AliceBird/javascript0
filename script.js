328226526ba347391ffc6d7c4f1b2a1d2b023340
AliceBird <71247955+AliceBird@users.noreply.github.com>
2021-12-22 15:07:26 +0300

    1 lesson
    I did it

---
+++ script.js
@@ -0,0 +1,17 @@
+alert("Привет. Мир!");
+
+let money = 158;
+console.log(money);
+let income = 170000;
+console.log(income);
+let addExpenses = 20000;
+console.log(addExpenses);
+let deposit = 50000;
+console.log(deposit);
+let mission = "win?";
+console.log(mission);
+let period = 3;
+console.log(period);
+
+let text = "Text about anything";
+console.log(text)
