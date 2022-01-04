7ce4326e39ac7faa33870583a468802ccb81c23d
AliceBird <71247955+AliceBird@users.noreply.github.com>
2022-01-04 22:18:32 +0300

    add ex 3

---
+++ script3.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// let a = 5;
+// console.log('a: ', a);
+//
+// // let question = confirm('тебе есть 118 лет?');
+// // console.log(question);
+//
+// // console.log(confirm('тебе есть 118 лет?'));
+//
+// let question2 = prompt('How old are you?', '118');
+// console.log(question2);
