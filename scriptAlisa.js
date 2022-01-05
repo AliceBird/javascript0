f7d6bafc174dc29e36d762d4fd3704d7dafd2bdf
AliceBird <71247955+AliceBird@users.noreply.github.com>
2022-01-05 18:20:30 +0300

    Create scriptAlisa.js

---
+++ scriptAlisa.js
@@ -0,0 +1,10 @@
+let string = 'Alisa';
+
+for (let i=0; i < string.length; i++ ){
+
+    // numResult *= numToString[i]
+//     // numResult = numResult * numToString[i]   // 7 и 8 строка одно и тоже
+
+
+    console.log(string[i]);
+}
