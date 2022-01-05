3b639b64b92ab3f67f25f66b038942d1e3637c97
AliceBird <71247955+AliceBird@users.noreply.github.com>
2022-01-05 14:47:20 +0300

    finished ex 2.2

--- script2.2.js
+++ script2.2.js
@@ -1,18 +1,39 @@
 let num = 266219;
-// let mult = 1;
-let digits = [];
-while (num > 0) {
-    digits.push(num % 10);
-    num = parseInt(num / 10);
-}
+let numToString = `${num}`;
+let numResult = 1;
+let numResult3 = null;
+for (let i=0; i < numToString.length; i++ ){
 
-digits.reverse();
-for (let i=0;i<massiv.length; i++){
-    // let sum = 1;
-    if (num[i]==1){
-    sum *= num[i];
-    console.log(digits);
-}}
+ numResult *= numToString[i]
+    // numResult = numResult * numToString[i]   // 7 и 8 строка одно и тоже
+}
+numResult3 = numResult **3;
+let toPrint = `${numResult3}`;
+
+console.log(numResult3);
+console.log(toPrint[0] + toPrint[1]);
+
+// 7 и 8 строка одно и тоже
+//домножаю, перевожу в строку и беру первый(0) элемент и второй(1) и вывожу в консоль
+//
+// digits.reverse();
+// for (let i=0;i<massiv.length; i++){
+   // новая переменная    при каких услвиях будет повторяться   После каждой итерации к ш будет добавлена 1 (= i=i+1)
+//     // let sum = 1;
+//     if (num[i]==1){
+//     sum *= num[i];
+//     console.log(digits);
+// }}
+
+// из числа сделать строку ,         for оператор который позволяет пробегаться по массиву , обьекту строке
+
+//пробежаться for  по полученной строке, на каждой итерации множить результат на число
+//
+// возвести в 3 степень   n**3  n**4
+//  переделать число в строку и вынуть первые две цифры
+
+
+
 // let arr = Array.from(num);
 // console.log(arr);
 // let numsplit = num.split;
@@ -25,3 +46,11 @@ for (let i=0;i<massiv.length; i++){
 // Избавиться от последнего разряда числа n путем деления нацело на 10!!!убираем последнюю цифру числа
 
 /*возвести в степень 3 не используя Math.pow*/
+
+
+// let mult = 1;
+// let digits = [];
+// while (num > 0) {
+//     digits.push(num % 10); частные случаи не в этом примере
+//     num = parseInt(num / 10);
+// }
