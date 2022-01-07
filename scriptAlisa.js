91a9f3b898622493ef2630fdfe3f8c678938fecc
AliceBird <71247955+AliceBird@users.noreply.github.com>
2022-01-07 10:56:05 +0300

    finished

--- scriptAlisa.js
+++ scriptAlisa.js
@@ -1,10 +1,10 @@
 let string = 'Alisa';
-
-for (let i=0; i < string.length; i++ ){
+string1 = string +'!';
+for (let i=0; i < string1.length; i++ ){
 
     // numResult *= numToString[i]
 //     // numResult = numResult * numToString[i]   // 7 и 8 строка одно и тоже
 
 
-    console.log(string[i]);
+    console.log(string1[i]);
 }
