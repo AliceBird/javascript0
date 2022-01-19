75e979bb16be442d7ffead6f1c907739cb09fede
AliceBird <71247955+AliceBird@users.noreply.github.com>
2022-01-19 11:03:32 +0300

    add

---
+++ script4.js
@@ -0,0 +1,18 @@
+'User strict'
+// const doNum = function (a, b, callback) {
+//    if (typeof  a === 'number' && typeof b === 'number') {
+// callback(a, b);
+// }
+// };
+//
+// doNum(5, 10, function f(a, b) {
+// // console.log(a + b);
+// });
+//
+// function mult (a, b) {
+//     console.log(a * b)
+// };
+// doNum(3, 5, mult);
+// // console.log(a * b)
+
+
