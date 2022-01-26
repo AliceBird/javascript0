ad860a92d8e3155580255837474c1d8cb7309188
AliceBird <71247955+AliceBird@users.noreply.github.com>
2022-01-26 12:18:18 +0300

    finished

--- scriptex5hard.js
+++ scriptex5hard.js
@@ -6,24 +6,39 @@
 //
 // — Вывести в консоль только те, что начинаются
 // с цифры 2 или 4 (Должны присутствовать в массиве)
-
-const arr = ['2516', '36582', '222', '684', '49455', '6958', '42988'];
-const arr1 = ['2516'];
 // создать массив
 //перебрать каждое значение массива
 //проверить первую цифру каждого числа
 //если начинается с 2 или 4
 //вывести кв консоль
 
-const getNumbers = (array) => {
-    for (let i = 0; i < array.length; i++) {
-        if (array[i][0] == 2 || array[i][0] == 4) {
-            console.log(array[i]);
-        }
-    }
-}
-getNumbers(arr);
-getNumbers(arr1);
+// const arr = ['2516', '36582', '222', '684', '49455', '6958', '42988'];
+// const arr1 = ['2516'];
+//
+// const getNumbers = (array) => {
+//     for (let i = 0; i < array.length; i++) {
+//         if (array[i][0] == 2 || array[i][0] == 4) {
+//             console.log(array[i]);
+//         }
+//     }
+// }
+// getNumbers(arr);
+// getNumbers(arr1);
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+// const String = ['monkey', 'elephant', 'cat', 'dog', 'tiger'];
+// findLongestWord = () => {
+//     let LongestWord = 0;
+//     for (let i = 0; i < String.length; i++){
+//         if (String[i].length > LongestWord) {
+//             LongestWord = String[i].length;
+//         }
+//         // console.log(String[i]);
+//     }
+//     return LongestWord;
+// }
+// findLongestWord();
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 //
 // function array(arr, elem) {
 //     for (let p = 2; p == arr.lenght;i++){
@@ -45,25 +60,35 @@ getNumbers(arr1);
 // // — Рядом с каждым числом написать оба делителя данного числа
 // //
 // //     Например: “Делители этого числа: 1 и n”
-let n = 100;
+
 // перебрать от 1 до определенного чилса
 // условия делители этого числа 1 и n
 // вывести в консоль число и “Делители этого числа: 1 и n”
 // %
 // console.log(14 % 5)
-const getComonNumber = () => {
+
+
+let n = 2;
+
+const getComonNumber = (n) => {
     let m = 0;
     for (let i = 2; i < n; i++) {
         if (n % i === 0) {
             m += 1
         }
     }
-    console.log(m);
-    if ( m === 0 ) {
-        console.log('Делители этого числа: 1 и n', n);
+    // if (m.lenght <= 2)
+    if (m === 0) {
+        console.log('Делители этого числа: 1 и', n);
     }
 }
-
-getComonNumber();
-
+getComonNumber(3);
+//перебрать все числа от 1 до 100
+//для каждого числа запустить функцию getcomonNumber
+let newFunction = (x) => {
+    for (let k = 1; k <= x; k++ ) {
+        getComonNumber(k)
+    }
+}
+newFunction(10)
 
