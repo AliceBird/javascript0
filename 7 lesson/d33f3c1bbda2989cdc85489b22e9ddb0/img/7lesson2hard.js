ea2a4eb1ded4ae65883d87f651f534d17df159c5
AliceBird <71247955+AliceBird@users.noreply.github.com>
2022-02-16 13:34:07 +0300

    add

--- 7 lesson/d33f3c1bbda2989cdc85489b22e9ddb0/img/7lesson2hard.js
+++ 7 lesson/d33f3c1bbda2989cdc85489b22e9ddb0/img/7lesson2hard.js
@@ -41,27 +41,59 @@ console.log(additionalIncomeItem);
 // начиная с class="budget_day-value" и
 // заканчивая class="target_month-value">)
 
-let target_month = document.querySelectorAll('.additional_income-item');
+let target_month = document.querySelectorAll('.value');
 console.log(target_month);
 
-let income_period = document.querySelectorAll('.additional_income-item');
-console.log(income_period);
-
-let additional_expenses = document.querySelectorAll('.additional_income-item');
-console.log(additional_expenses);
-
-let additional_income = document.querySelectorAll('.additional_income-item');
-console.log(additional_income);
-
-
-let expenses_month = document.querySelectorAll('.additional_income-item');
-console.log(expenses_month);
-
-let budget_day = document.querySelectorAll('.additional_income-item');
-console.log(budget_day);
-
-let budget_month = document.querySelectorAll('.additional_income-item');
-console.log(budget_month);
+// let income_period = document.querySelectorAll('.additional_income-item');
+// console.log(income_period);
+//
+// let additional_expenses = document.querySelectorAll('.additional_income-item');
+// console.log(additional_expenses);
+//
+// let additional_income = document.querySelectorAll('.additional_income-item');
+// console.log(additional_income);
+//
+//
+// let expenses_month = document.querySelectorAll('.additional_income-item');
+// console.log(expenses_month);
+//
+// let budget_day = document.querySelectorAll('.additional_income-item');
+// console.log(budget_day);
+//
+// let budget_month = document.querySelectorAll('.additional_income-item');
+// console.log(budget_month);
 
 //Получить оставшиеся поля через querySelector каждый в отдельную переменную
 // (Инпуты с левой стороны не забудьте про range)
+let periodSelect = document.querySelectorAll('period-select');
+console.log();
+
+let targetAmount = document.querySelectorAll('target-amount');
+console.log(targetAmount);
+
+let depositPercent = document.querySelectorAll('deposit-percent');
+console.log(depositPercent);
+
+let depositAmount = document.querySelectorAll('deposit-amount');
+console.log(depositAmount);
+
+let additionalExpensesTitle = document.querySelectorAll('additional_expenses-title');
+console.log(additionalExpensesTitle);
+
+let expensesAmount = document.querySelectorAll('expenses-amount');
+console.log(expensesAmount);
+
+let expensesTitle = document.querySelectorAll('expenses-title');
+console.log(expensesTitle);
+
+let addiIncomeItem = document.querySelectorAll('additional_income-item');
+console.log(addiIncomeItem);
+
+let incomeAmount = document.querySelectorAll('income-amount');
+console.log(incomeAmount);
+
+let incomeTitle = document.querySelectorAll('income-title');
+console.log(incomeTitle);
+
+let salaryAmount = document.querySelectorAll('salary-amount');
+console.log(salaryAmount);
